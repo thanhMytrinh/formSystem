8c290fab5447eea2b9e78638e6d95ba5f7c95d5a
thanhmy.trinh <thanhmy.trinh03@gmail.com>
2026-04-30 04:49:01 +0700

    Initial commit

---
+++ formSystem/src/main/java/com/formSystem/formSystem/FormSystemApplication.java
@@ -0,0 +1,13 @@
+package com.formSystem.formSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSystemApplication.class, args);
+	}
+
+}
---
+++ formSystem/src/test/java/com/formSystem/formSystem/FormSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formSystem.formSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
