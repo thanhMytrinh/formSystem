88f0fc89c10ccd02935ed13dab8a887c4b2c520f
thanhmy.trinh <thanhmy.trinh03@gmail.com>
2026-04-30 13:46:08 +0700

    add swagger configuruation

---
+++ formSystem/src/main/java/com/formSystem/formSystem/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.formSystem.formSystem.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Form Management System API")
+                        .description("Hệ thống quản lý form cho Admin và Nhân viên SW"));
+
+    }
+}
